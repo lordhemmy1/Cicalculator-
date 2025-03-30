425ac3a21cd22d8df415068745ab37df4c8f85f5
lordhemmy1 <96589103+lordhemmy1@users.noreply.github.com>
2025-03-30 20:17:26 +0100

    Update calculator.php

--- calculator.php
+++ calculator.php
@@ -12,7 +12,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $time = 30;
     $result = "";
 }
-?><!DOCTYPE html><html lang="en">
+?>
+<!DOCTYPE html><html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
